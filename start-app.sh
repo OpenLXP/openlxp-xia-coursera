cf698e5b7d035a914794c0b69d37ba489b0061cf
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2025-01-30 09:24:23 -0800

    Modifying start up scripts

--- start-app.sh
+++ start-app.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 # start-server.sh
 
+cd /tmp/app/openlxp-xia-coursera/
+sed -i 's/hashlib.md5()/hashlib.md5(usedforsecurity=False)/g' /tmp/app/.cache/python-packages/django/db/backends/utils.py
 python manage.py waitdb 
-python manage.py migrate 
+python manage.py migrate --skip-checks
 python manage.py createcachetable 
 python manage.py loaddata admin_theme_data.json 
 python manage.py loaddata openlxp_email.json 
 python manage.py loaddata openlxp_notification_template.json 
 python manage.py loaddata openlxp_notification_subject.json 
 python manage.py loaddata openlxp_notification_email.json 
+python manage.py collectstatic --no-input
+cp -ur ./static/ /tmp/shared/
+cp -ur ./media/ /tmp/shared/
 cd /tmp/app/ 
 if [ -n "$TMP_SOURCE_DIR" ] ; then 
     (cd openlxp-xia-coursera; install -d -o www-data -p $TMP_SOURCE_DIR) 
--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,4 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd openlxp-xia-coursera; python manage.py createsuperuser --no-input)
 fi
-(cd openlxp-xia-coursera; gunicorn openlxp_xia_coursera_project.wsgi --reload --user www-data --bind 0.0.0.0:8010 --workers 3) &
-nginx -g "daemon off;"
+(cd openlxp-xia-coursera; gunicorn openlxp_xia_coursera_project.wsgi --reload --user python --bind 0.0.0.0:8020 --workers 3)
